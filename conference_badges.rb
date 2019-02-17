a6fd656af84e8d13bab4d10227bf8657d436b7c3
Ryan Pethel <arpethel@gmail.com>
2019-02-17 16:38:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(names_array)
+  messages_array = []
+
+  names_array.each do |name|
+    messages_array << badge_maker(name)
+  end
+  return messages_array
+end
